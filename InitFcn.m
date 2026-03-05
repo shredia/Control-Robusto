ee0bd32aedfce7a398e45cecb43b041dc6092ac1
shredia <36351492+shredia@users.noreply.github.com>
2026-03-05 19:37:38 -0300

    Bumpless transfer, Funcionamiento del EKF+DO
    -Se agregó el bumpless transfer, considerando integradores de velocidad (Iq actual) y de corriente(Voltaje aplicado).
    -Se modificó J y B, se agregaron J_Var y B_var mayores para mayor robustez.

--- InitFcn.m
+++ InitFcn.m
@@ -19,10 +19,17 @@ Ke = Psi;
 max_step_rate = 3000;
 R =  3.4; %% [Ohms]
 L = 6/1000; %% [L]
+%%Propiedades del motor (variables que creo conocer)
 B_internal = 5/10000; %%roce
 J_internal = 4.7/1000000; %%inercia del motor 
-J_external = J_internal*0.1;
-B_external = B_internal*0.2;
+%%Propiedades de carga del motor (variables que desconozco)
+J_external = J_internal*1;
+B_external = B_internal*1;
+%%Variables del modelo (Variables que asumo más grandes) 
+J_var = J_internal*10;
+B_var = B_internal*10;
+
+%%Variables de roce que desconozco
 Tc = 0.002; %%Coulomb friction
 Tba = 2.5*Tc; %%Breakwat friction
 Wba = 2;%%breakway frictions (rad/s)
@@ -34,7 +41,7 @@ InitialSpeed = 0; %%rad/s
 
 Vdc = 24;
 
-fbw = 500;          % Hz (BW corriente)
+fbw = 800;          % Hz (BW corriente)
 wd  = 2*pi*fbw;      % rad/s  <-- CORRECTO
 
 f_carrier = 40e3;
@@ -42,7 +49,7 @@ frecuency_simulation = 100e3;
 sample_time = 1/frecuency_simulation;
 
 
-f_current = fbw*10;
+f_current = f_carrier;
 f_Wm = 30;
 f_ekf = f_current;
 
@@ -50,7 +57,7 @@ Ts_ekf = 1/f_ekf;
 
 
 Ts_current = 1/f_current;
-Ts_Wm = 1/(500);
+Ts_Wm = Ts_current*10;
 Ts_DO = Ts_Wm;
 Kp_d = wd*L;
 Ki_d = wd*R;
@@ -61,11 +68,11 @@ Ki_q = wd*R;
 shi_w = 1.5;
 wn_w = 2*pi*f_Wm
 
-Kp_w = (2*shi_w*wn_w*J_internal)/Kt;
-Ki_w = (wn_w^2)*J_internal/Kt;
+Kp_w = (2*shi_w*wn_w*J_var)/Kt;
+Ki_w = (wn_w^2)*J_var/Kt;
 
 % Si B desconocido, no uses B0,B1 con (B/J) real
-BJ_hat = 0;     % o una estimación
+BJ_hat = 0.5;     % o una estimación
 B0 = 3*wn_w - BJ_hat;
 B1 = 3*wn_w^2 - B0*BJ_hat;
-B2 = -(J_internal/P)*(wn_w^3);
+B2 = -(J_var/P)*(wn_w^3);
